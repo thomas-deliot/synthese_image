5b08929caef3133876f0c3c2f4a346e24a5bc729
theFrenchDutch <thomasdeliot@gmail.com>
2017-11-09 09:26:09 +0100

    indent

--- Shaders/ssr_fx.glsl
+++ Shaders/ssr_fx.glsl
@@ -45,89 +45,89 @@ float DistanceSquared(vec2 a, vec2 b)
 bool FindSSRHit(vec3 csOrig, vec3 csDir, float jitter,
 	out vec2 hitPixel, out vec3 hitPoint) 
 {
-    // Clip to the near plane
-    float rayLength = ((csOrig.z + csDir.z * maxDistance) > -nearZ) ?
-        (-nearZ - csOrig.z) / csDir.z : maxDistance;
-    vec3 csEndPoint = csOrig + csDir * rayLength;
- 
-    // Project into homogeneous clip space
-    vec4 H0 = projToPixel * vec4(csOrig, 1.0);
-    vec4 H1 = projToPixel * vec4(csEndPoint, 1.0);
-    float k0 = 1.0 / H0.w, k1 = 1.0 / H1.w;
- 
-    // The interpolated homogeneous version of the camera-space points  
-    vec3 Q0 = csOrig * k0, Q1 = csEndPoint * k1;
- 
-    // Screen-space endpoints
+	// Clip to the near plane
+	float rayLength = ((csOrig.z + csDir.z * maxDistance) > -nearZ) ?
+		(-nearZ - csOrig.z) / csDir.z : maxDistance;
+	vec3 csEndPoint = csOrig + csDir * rayLength;
+
+	// Project into homogeneous clip space
+	vec4 H0 = projToPixel * vec4(csOrig, 1.0);
+	vec4 H1 = projToPixel * vec4(csEndPoint, 1.0);
+	float k0 = 1.0 / H0.w, k1 = 1.0 / H1.w;
+
+	// The interpolated homogeneous version of the camera-space points  
+	vec3 Q0 = csOrig * k0, Q1 = csEndPoint * k1;
+
+	// Screen-space endpoints
 	vec2 P0 = H0.xy * k0, P1 = H1.xy * k1;
- 
-    // If the line is degenerate, make it cover at least one pixel
-    // to avoid handling zero-pixel extent as a special case later
-    P1 += vec2((DistanceSquared(P0, P1) < 0.0001) ? 0.01 : 0.0);
-    vec2 delta = P1 - P0;
- 
-    // Permute so that the primary iteration is in x to collapse
-    // all quadrant-specific DDA cases later
-    bool permute = false;
-    if (abs(delta.x) < abs(delta.y)) 
-	{ 
-        // This is a more-vertical line
-        permute = true;
+
+	// If the line is degenerate, make it cover at least one pixel
+	// to avoid handling zero-pixel extent as a special case later
+	P1 += vec2((DistanceSquared(P0, P1) < 0.0001) ? 0.01 : 0.0);
+	vec2 delta = P1 - P0;
+
+	// Permute so that the primary iteration is in x to collapse
+	// all quadrant-specific DDA cases later
+	bool permute = false;
+	if (abs(delta.x) < abs(delta.y)) 
+	{
+		// This is a more-vertical line
+		permute = true;
 		delta = delta.yx;
 		P0 = P0.yx;
 		P1 = P1.yx; 
-    }
- 
-    float stepDir = sign(delta.x);
-    float invdx = stepDir / delta.x;
- 
-    // Track the derivatives of Q and k
-    vec3  dQ = (Q1 - Q0) * invdx;
-    float dk = (k1 - k0) * invdx;
-    vec2  dP = vec2(stepDir, delta.y * invdx);
- 
-    // Scale derivatives by the desired pixel stride and then
-    // offset the starting values by the jitter fraction
-    dP *= stride; dQ *= stride; dk *= stride;
-    P0 += dP * jitter; Q0 += dQ * jitter; k0 += dk * jitter;
- 
-    // Slide P from P0 to P1, (now-homogeneous) Q from Q0 to Q1, k from k0 to k1
-    vec3 Q = Q0; 
- 
-    // Adjust end condition for iteration direction
-    float end = P1.x * stepDir;
- 
-    float k = k0, stepCount = 0.0, prevZMaxEstimate = csOrig.z;
-    float rayZMin = prevZMaxEstimate, rayZMax = prevZMaxEstimate;
-    float sceneZMax = rayZMax + 100;
-    for (vec2 P = P0; 
-         ((P.x * stepDir) <= end) && (stepCount < maxSteps) &&
+	}
+	
+	float stepDir = sign(delta.x);
+	float invdx = stepDir / delta.x;
+	
+	// Track the derivatives of Q and k
+	vec3  dQ = (Q1 - Q0) * invdx;
+	float dk = (k1 - k0) * invdx;
+	vec2  dP = vec2(stepDir, delta.y * invdx);
+
+	// Scale derivatives by the desired pixel stride and then
+	// offset the starting values by the jitter fraction
+	dP *= stride; dQ *= stride; dk *= stride;
+	P0 += dP * jitter; Q0 += dQ * jitter; k0 += dk * jitter;
+
+	// Slide P from P0 to P1, (now-homogeneous) Q from Q0 to Q1, k from k0 to k1
+	vec3 Q = Q0; 
+
+	// Adjust end condition for iteration direction
+	float end = P1.x * stepDir;
+
+	float k = k0, stepCount = 0.0, prevZMaxEstimate = csOrig.z;
+	float rayZMin = prevZMaxEstimate, rayZMax = prevZMaxEstimate;
+	float sceneZMax = rayZMax + 100;
+	for (vec2 P = P0; 
+		((P.x * stepDir) <= end) && (stepCount < maxSteps) &&
 			((rayZMax < sceneZMax - zThickness) || (rayZMin > sceneZMax)) &&
 			(sceneZMax != 0); 
-         P += dP, Q.z += dQ.z, k += dk, ++stepCount) 
+			P += dP, Q.z += dQ.z, k += dk, ++stepCount) 
 	{
-        rayZMin = prevZMaxEstimate;
-        rayZMax = (dQ.z * 0.5 + Q.z) / (dk * 0.5 + k);
-        prevZMaxEstimate = rayZMax;
-        if (rayZMin > rayZMax) 
+		rayZMin = prevZMaxEstimate;
+		rayZMax = (dQ.z * 0.5 + Q.z) / (dk * 0.5 + k);
+		prevZMaxEstimate = rayZMax;
+		if (rayZMin > rayZMax) 
 		{ 
-           float t = rayZMin;
-		   rayZMin = rayZMax;
-		   rayZMax = t;
-        }
+			float t = rayZMin;
+			rayZMin = rayZMax;
+			rayZMax = t;
+		}
  
-        hitPixel = permute ? P.yx : P;
-        // You may need hitPixel.y = renderSize.y - hitPixel.y; here if your vertical axis
-        // is different than ours in screen space
+		hitPixel = permute ? P.yx : P;
+		// You may need hitPixel.y = renderSize.y - hitPixel.y; here if your vertical axis
+		// is different than ours in screen space
 		//hitPixel.y = renderSize.y - hitPixel.y;
 		float tempZ = (2 * nearZ) / (farZ + nearZ - texture(depthBuffer, hitPixel / renderSize).x * (farZ - nearZ));
 		sceneZMax = -(tempZ * (farZ - nearZ) + nearZ);
-    }
-     
-    // Advance Q based on the number of steps
-    Q.xy += dQ.xy * stepCount;
-    hitPoint = Q * (1.0 / k);
-    return (rayZMax >= sceneZMax - zThickness) && (rayZMin < sceneZMax);
+	}
+
+	// Advance Q based on the number of steps
+	Q.xy += dQ.xy * stepCount;
+	hitPoint = Q * (1.0 / k);
+	return (rayZMax >= sceneZMax - zThickness) && (rayZMin < sceneZMax);
 }
 
 void main()
